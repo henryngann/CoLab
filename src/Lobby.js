ebd4589b02112e18a545eb555e38af9dc6d2d694
henryngann <henrynganwork@gmail.com>
2021-02-06 22:21:23 -0500

    some lists

--- src/Lobby.js
+++ src/Lobby.js
@@ -139,7 +139,39 @@ const Lobby = ({
         <div>
           <br />
           <h2>Your Workouts</h2>
-          <p>List of Workouts PlaceHolder</p>
+          <div className="list-group">
+            <button
+              type="button"
+              className="list-group-item list-group-item-action"
+              aria-current="true"
+            >
+              Cras justo odio
+            </button>
+            <button
+              type="button"
+              className="list-group-item list-group-item-action"
+            >
+              Dapibus ac facilisis in
+            </button>
+            <button
+              type="button"
+              className="list-group-item list-group-item-action"
+            >
+              Morbi leo risus
+            </button>
+            <button
+              type="button"
+              className="list-group-item list-group-item-action"
+            >
+              Porta ac consectetur ac
+            </button>
+            <button
+              type="button"
+              className="list-group-item list-group-item-action"
+            >
+              Vestibulum at eros
+            </button>
+          </div>
         </div>
       ) : (
         ""
