4bb20ec60b3f4b32bab71c925d1d65db6d8ab3b1
Lily <lilywuyanru@gmail.com>
2021-03-26 23:14:53 -0400

    update default workout list

--- src/Home/CreateRoom/CreateRoom.js
+++ src/Home/CreateRoom/CreateRoom.js
@@ -84,116 +84,87 @@ const CreateRoom = () => {
   )
 
   return (
-    <form onSubmit={handleSubmit}>
+    <>
+      <form onSubmit={handleSubmit}>
+        <br />
+        {/* {roomState === "make_youtube" ? makeYoutubeMarkup : (
+          ""
+        )} */}
+        <div style={{ position: "relative", left: "-19rem"}}>
+          <div className="form-floating mb-3 ">
+            <input
+              type="text"
+              className="form-control bradius"
+              id="field"
+              placeholder="Name"
+              value={roomTitle}
+              style={{
+                width: "25rem",
+                paddingBottom: "25px",
+                paddingTop: "35px",
+              }}
+              onChange={handleRoomTitle}
+              readOnly={connecting}
+              required
+            />
+            <label htmlFor="name">Enter your Room Title</label>
+          </div>
+
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control bradius"
+              id="field"
+              style={{
+                width: "25rem",
+                height: "2.5rem",
+                paddingBottom: "25px",
+                paddingTop: "35px",
+              }}
+              placeholder="Password"
+              readOnly={connecting}
+            />
+            <label htmlFor="name">Room Password (optional)</label>
+          </div>
+
+          <div className="form-floating mt-5">
+            <input
+              type="text"
+              className="form-control bradius"
+              id="room"
+              placeholder="Room Code"
+              value={roomName}
+              readOnly={true}
+              required
+            />
+            <label htmlFor="room">Room Code</label>
+          </div>
+          <button
+            style={{
+              position: "relative",
+              left: "16rem",
+              width: "10rem",
+              top: "-3.6rem",
+              height: "3.7rem",
+            }}
+            className="text-align-center arrowIcon"
+            type="submit"
+          >
+            {rightElement}
+          </button>
+        </div>
+      </form>
+
+      <div className="workout-list">
       <br />
-      {/* {roomState === "make_youtube" ? makeYoutubeMarkup : (
-        ""
-      )} */}
-      <div style={{ position: "relative", left: "-19rem" }}>
-        <div className="form-floating mb-3 ">
-          <input
-            type="text"
-            className="form-control bradius"
-            id="field"
-            placeholder="Name"
-            value={roomTitle}
-            style={{
-              width: "25rem",
-              paddingBottom: "25px",
-              paddingTop: "35px",
-            }}
-            onChange={handleRoomTitle}
-            readOnly={connecting}
-            required
-          />
-          <label htmlFor="name">Enter your Room Title</label>
-        </div>
-
-        <div className="form-floating mb-3">
-          <input
-            type="text"
-            className="form-control bradius"
-            id="field"
-            style={{
-              width: "25rem",
-              height: "2.5rem",
-              paddingBottom: "25px",
-              paddingTop: "35px",
-            }}
-            placeholder="Password"
-            readOnly={connecting}
-          />
-          <label htmlFor="name">Room Password (optional)</label>
-        </div>
-
-        <div className="form-floating mt-5">
-          <input
-            type="text"
-            className="form-control bradius"
-            id="room"
-            placeholder="Room Code"
-            value={roomName}
-            readOnly={true}
-            required
-          />
-          <label htmlFor="room">Room Code</label>
-        </div>
-        <button
-          style={{
-            position: "relative",
-            left: "16rem",
-            width: "10rem",
-            top: "-3.6rem",
-            height: "3.7rem",
-          }}
-          className="text-align-center arrowIcon"
-          type="submit"
-        >
-          {rightElement}
+      <h2>Your Workouts</h2>
+      <div className="list-group">
+        <button type="button">
+          Cras justo odio
         </button>
       </div>
-      {roomState === "make_custom" ? (
-        <div>
-          <br />
-          <h2>Your Workouts</h2>
-          <div className="list-group">
-            <button
-              type="button"
-              className="list-group-item list-group-item-action"
-              aria-current="true"
-            >
-              Cras justo odio
-            </button>
-            <button
-              type="button"
-              className="list-group-item list-group-item-action"
-            >
-              Dapibus ac facilisis in
-            </button>
-            <button
-              type="button"
-              className="list-group-item list-group-item-action"
-            >
-              Morbi leo risus
-            </button>
-            <button
-              type="button"
-              className="list-group-item list-group-item-action"
-            >
-              Porta ac consectetur ac
-            </button>
-            <button
-              type="button"
-              className="list-group-item list-group-item-action"
-            >
-              Vestibulum at eros
-            </button>
-          </div>
-        </div>
-      ) : (
-        ""
-      )}
-    </form>
+    </div>
+    </>
   );
 };
 
